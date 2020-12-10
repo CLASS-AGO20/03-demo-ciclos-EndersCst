04bdb979abe820dfceb909deb53534c2e4152355
EndersCst <73366481+EndersCst@users.noreply.github.com>
2020-12-10 15:38:20 -0600

    implemented sumarParesFor()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,54 @@
-export default class App {}
+export default class App {
+//presentación del ciclo For
+    sumarParesFor() {
+    for(let i = 1; i<=20; i = i + 1) {
+        console.log(i);
+    }
 
+    console.log("Terminó el ciclo creciente");
+    }
+restarparesFor(){
+    for (let i = 25; i>=5;i = i-1){
+        console.log(i);
+    }
+
+    console.log("terminó el ciclo decreciente");
+}
+//ejercicio1 03a
+sumarParesFor(){
+    let suma = 0;
+
+    for(let i =1; i<=20; i =i + 1){
+        if(i%2 === 0) {
+            suma =suma + i;
+        }
+    }
+    return suma;
+}
+//ejercicio2
+contarImparesFor(){
+
+}
+
+//ejercicio3
+sumarParesWhile(){
+let i = 1;
+let suma = 0;
+
+    while(i<= 20){
+        if(1&2===0){
+            suma = suma + i;
+        }
+    i = i + 1;
+}
+return suma;
+}
+}
 let app = new App();
+
+app.sumarParesFor();
+app.restarparesFor();
+
+console.log(app.sumarParesFor());
+
+console.log(app.sumarParesWhile());
